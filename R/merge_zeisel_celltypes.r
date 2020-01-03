8a1e35ff352e31dbb34c03c0e402fbb97330439a
NathanSkene <n.skene@imperial.ac.uk>
2020-01-03 08:49:26 +0000

    Setting up. Leaving out renv.

---
+++ R/merge_zeisel_celltypes.r
@@ -0,0 +1,26 @@
+#' Given a list of data from each celltype, keep only the matrices from the list corresponding to cells in useCells
+#'
+#' \code{merge_zeisel_celltypes}
+#'
+#' @param allExp A list, where each entry has a matrix
+#' @return A list containing $exp with the sparse matrix and $annot with annot data
+#' @examples
+#' # create a temporary directory
+#' td = tempdir()
+#'  create the placeholder file
+#' tf = tempfile(tmpdir=td, fileext=".zip")
+#' download into the placeholder file
+#' download.file("https://storage.googleapis.com/linnarsson-lab-loom/l5_all.loom", tf)
+#' zeisel2018 = prep_zeisel2018()
+#' keptExp = merge_zeisel_celltypes(allExp,useCells=c("TEGLU1","TEGLU2","MGL1","MOL1"))
+#' @export
+merge_zeisel_celltypes <- function(allExp,useCells=c("TEGLU1","TEGLU2","MGL1","MOL1")){
+  exp = allExp[[useCells[1]]]
+  colnames(exp) = rep(useCells[1],dim(exp)[2])
+  for(ct in useCells[-1]){
+    tmp = allExp[[ct]]
+    colnames(tmp) = rep(ct,dim(tmp)[2])
+    exp = cbind(exp,tmp)
+  }
+  return(exp)
+}
---
+++ R/split_celltypes_byIndv.r
@@ -0,0 +1,31 @@
+split_celltypes_byIndv <- function(keptExp,joinCells=c("TEGLU1","TEGLU2"),nCases=3,jointName="Pyramidal"){
+  # joinCells[1] are the 'cases' and joinCells[2] are the 'controls'
+  if(length(joinCells)!=2){stop("joinCells should be only two names")}
+  otherCells = setdiff(unique(colnames(keptExp)),joinCells)
+  idx_otherCells = which(colnames(keptExp) %in% otherCells)
+  sampleGroups = split(idx_otherCells, c("Cases","Controls"))
+  idx_cases = c(sampleGroups$Cases,which(colnames(keptExp) %in% joinCells[1]))
+  idx_controls = c(sampleGroups$Controls,which(colnames(keptExp) %in% joinCells[2]))
+  colnames(keptExp)[colnames(keptExp) %in% joinCells] = jointName
+
+  # Setup the case/control labels
+  dx = rep("Cases",dim(keptExp)[2])
+  dx[idx_controls] = "Controls"
+
+  # Setup the individual labels
+  indv_cases    = split(which(dx=="Cases"), 1:(nCases))
+  indv_controls = split(which(dx=="Controls"), 1:(nCases))
+  indv = c(indv_cases,indv_controls)
+
+  # Create new list
+  outputList = list()
+  for(ii in 1:length(indv)){
+    outputList[[ii]] = list()
+    outputList[[ii]]$exp = keptExp[,indv[[ii]]]
+    outputList[[ii]]$dx  = unique(dx[indv[[ii]]])
+    outputList[[ii]]$ct  = colnames(outputList[[ii]]$exp)
+    N = length(colnames(outputList[[ii]]$exp))
+    colnames(outputList[[ii]]$exp) = sprintf("%s_%s",colnames(outputList[[ii]]$exp),1:N)
+  }
+  return(outputList)
+}
